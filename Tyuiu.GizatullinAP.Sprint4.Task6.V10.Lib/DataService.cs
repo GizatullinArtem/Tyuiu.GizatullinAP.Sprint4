7e986c137f64048479a1f1004fe6ac172dd18c4b
coc1986data@gmail.com <coc1986data@gmail.com>
2024-11-27 18:53:25 +0500

    Task6

--- Tyuiu.GizatullinAP.Sprint4.Task6.V10.Lib/DataService.cs
+++ Tyuiu.GizatullinAP.Sprint4.Task6.V10.Lib/DataService.cs
@@ -1,7 +1,8 @@
-﻿using tyuiu.cources.programming.interfaces;
+﻿
+using tyuiu.cources.programming.interfaces.Sprint4;
 namespace Tyuiu.GizatullinAP.Sprint4.Task6.V10.Lib
 {
-    public class DataService 
+    public class DataService : ISprint4Task6V10
     {
         public string[] Calculate(string[] array)
         {
