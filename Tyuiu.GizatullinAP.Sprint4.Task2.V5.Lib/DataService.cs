9a3e350e353f32a9b09ee7321aa07c8ab7bfbde5
coc1986data@gmail.com <coc1986data@gmail.com>
2024-11-27 17:16:20 +0500

    Task2

---
+++ Tyuiu.GizatullinAP.Sprint4.Task2.V5.Lib/DataService.cs
@@ -0,0 +1,24 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+namespace Tyuiu.GizatullinAP.Sprint4.Task2.V5.Lib
+{
+    public class DataService : ISprint4Task2V5
+    {
+        public int Calculate(int[] array)
+        {
+            Random random = new Random();
+
+            int count = 14;
+            int[] numbers = new int[count];
+            int result = 1;
+
+            foreach (int num in array)
+            {
+                if (num % 2 == 0)
+                {
+                    result *= num;
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ Tyuiu.GizatullinAP.Sprint4.Task2.V5/Program.cs
@@ -0,0 +1,38 @@
+﻿using Tyuiu.GizatullinAP.Sprint4.Task2.V5.Lib;
+
+namespace Tyuiu.GizatullinAP.Sprint4.Task2.V5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #4 | Выполнил: Гизатуллин А. П. | ИСПб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Одномерные массивы (генератор случайных чисел)                    *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #5                                                              *");
+            Console.WriteLine("* Выполнил: Гизатуллин Артём Павлович  | ИСПб-24-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан одномерный целочисленный массив на 14 элементов заполненный         *");
+            Console.WriteLine("* лучайными в диапазоне от 3 до 9 подсчитать произведение четных          *");
+            Console.WriteLine("* элементов массива.                                                      *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int[] array = new int[] { 5, 5, 7, 4, 9, 5, 3, 5, 7, 5, 5, 4, 8, 5 };
+            int result = ds.Calculate(array);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ                                                               *");
+            Console.WriteLine("***************************************************************************");
+
+
+            Console.WriteLine($"Сумма всех четных чисел массива {string.Join(", ", array)} равна {result}");
+        }
+    }
+}
